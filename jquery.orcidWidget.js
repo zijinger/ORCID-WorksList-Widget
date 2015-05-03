d2ffb4c9df672868e998e29022810db3e9164646
dagrenzer <daryl.grenz@kaust.edu.sa>
2015-05-03 04:26:29 +0300

    Limit to retrieval of works list

--- jquery.orcidWidget.js
+++ jquery.orcidWidget.js
@@ -1,6 +1,6 @@
 var $jq = jQuery.noConflict()
 $jq(document).ready(function () {
-    var widget = $jq('#orcid-profiles-widget-js').clone(),
+    var widget = $jq('#orcid-widget-js').clone(),
         orcids = widget.data(orcids).orcids.split(","),
         profile = null;
     $jq(orcids).each(function (name, value) {
@@ -12,22 +12,10 @@ $jq(document).ready(function () {
         })
     })
     
-    $jq('#orcid-profiles-widget-js').replaceWith(widget)
-    function sort_profiles(wrapper) {
-      list = $jq(wrapper).children('.orcid-profile')
-      list.sort(function(a, b) {
-        if($jq(a).data('familyname') < $jq(b).data('familyname')) { return -1 }
-        if($jq(a).data('familyname') > $jq(b).data('familyname')) { return 1 }
-         return 0
-      })
-       return list.detach().appendTo(wrapper)
-    }
+    $jq('#orcid-widget-js').replaceWith(widget)
     
     function set_widget_content() {
         var profile_div = $jq('<div class="orcid-profile">')
-        profile_div.attr('data-name', person_name())
-        profile_div.attr('data-familyname', profile['orcid-bio']['personal-details']['family-name'].value)
-        set_bio().appendTo(profile_div)
         set_person_works().appendTo(profile_div)
         var orcid_uri = profile['orcid-identifier']['uri']
         var orcid_link = $jq("<a>")
@@ -40,23 +28,13 @@ $jq(document).ready(function () {
     }
 
     function get_orcid_profile(orcid) {
-        var profile_uri = 'http://pub.orcid.org/v1.1/' + orcid + '/orcid-profile';
+        var profile_uri = 'http://pub.orcid.org/v1.2/' + orcid + '/orcid-works';
         return $jq.ajax({
             url: profile_uri,
             type: 'GET',
             dataType: 'jsonp',
             accepts: 'application/orcid+json'
         })
-    }
-
-    function set_bio() {
-        var data = profile['orcid-bio']
-        var name = person_name(data);
-        var name_span = $jq('<span class="credit-name">');
-        $jq('<h1>' + name + '</h1>').appendTo(name_span);
-        var bio_span = $jq('<span class="orcid-bio">');
-        name_span.appendTo(bio_span);
-        return bio_span
     }
 
     function set_person_works() {
@@ -96,16 +74,5 @@ $jq(document).ready(function () {
             li.appendTo(ul);
         });
         return ul
-    }
-
-    function person_name() {
-        var data = profile['orcid-bio']
-        var name = data['personal-details']['credit-name']
-        if (name) {
-            name = name.value
-        } else {
-            name = data['personal-details']['given-names'].value + " " + data['personal-details']['family-name'].value;
-        }
-        return name
     }
 });
